761c3a2970f5bec5e6a0cfffba76bcf3a824865d
Gabriel Costa <gabrielszcosta@gmail.com>
2019-07-05 00:03:49 -0400

    Modificação para uso sem api

--- src/store/sagas/playlistDetails.js
+++ src/store/sagas/playlistDetails.js
@@ -1,16 +1,30 @@
 import { call, put } from "redux-saga/effects";
-import api from "../../services/api";
+//import api from "../../services/api";
+import { data } from "../../data";
 
 import { Creators as PlaylistDetailsActions } from "../ducks/playlistDetails";
 import { Creators as ErrorsActions } from "../ducks/error";
 
 export function* getPlaylistDetails(action) {
   try {
-    const { data } = yield call(
-      api.get,
-      `/playlists/${action.payload.id}?_embed=songs`
+    // const { data } = yield call(
+    //   api.get,
+    //   `/playlists/${action.payload.id}?_embed=songs`
+    // );
+
+    const playlists = data.playlists.filter(
+      playlist => playlist.id == action.payload.id
     );
-    yield put(PlaylistDetailsActions.getPlaylistDetailsSuccess(data));
+
+    const playlist = {
+      id: playlists[0].id,
+      description: playlists[0].description,
+      thumbnail: playlists[0].thumbnail,
+      title: playlists[0].title,
+      songs: data.songs.filter(song => song.playlistId === playlists[0].id)
+    };
+
+    yield put(PlaylistDetailsActions.getPlaylistDetailsSuccess(playlist));
   } catch (e) {
     yield put(
       ErrorsActions.setError("Não foi possível obter os detalhes da playlist")
--- src/store/sagas/playlists.js
+++ src/store/sagas/playlists.js
@@ -1,13 +1,14 @@
 import { call, put } from "redux-saga/effects";
-import api from "../../services/api";
+//import api from "../../services/api";
+import { data } from "../../data";
 
 import { Creators as PlaylistsActions } from "../ducks/playlists";
 import { Creators as ErrorsActions } from "../ducks/error";
 
 export function* getPlaylists() {
   try {
-    const { data } = yield call(api.get, "/playlists");
-    yield put(PlaylistsActions.getPlaylistsSuccess(data));
+    //const { data } = yield call(api.get, "/playlists");
+    yield put(PlaylistsActions.getPlaylistsSuccess(data.playlists));
   } catch (e) {
     yield put(ErrorsActions.setError("Não foi possível obter as playlists"));
   }
